c28243d4eff9f510fa4b9508c61a2b1003ddeca6
Dominik Wilk <mr.wilkd@gmail.com>
2023-07-13 19:58:48 +0100

    Add new function for infinity scroll

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
-const apiKey = 'wYWaXiDBtyC47GX3e7ECtbjjbAidSM0c-Ue6PWZR9g0';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const count = 30;
+// const apiKey = 'wYWaXiDBtyC47GX3e7ECtbjjbAidSM0c-Ue6PWZR9g0';
+const key1 = 'hlCwgSQfJUze6-qbz8voTfnVLrgZtWyLEVN7t8q_mZQ';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${key1}&count=${count}`;
+
+// Check if all images were loaded
+
+function imageLoaded() {
+	imagesLoaded++;
+	console.log(imagesLoaded);
+	if (imagesLoaded === totalImages) {
+		imagesLoaded = 0;
+		ready = true;
+		console.log('ready =', ready);
+	}
+}
 
 // Helper functtion to set attributes on DOM elements
 
@@ -19,6 +35,8 @@ function setAttributes(element, attributes) {
 // Create elements for links and photos
 
 function displayPhotos() {
+	totalImages = photosArray.length;
+	console.log('total images', totalImages);
 	// Run function for each object in photosArray
 	photosArray.forEach(photo => {
 		// Create <a> to link to Unsplash
@@ -35,6 +53,10 @@ function displayPhotos() {
 			alt: photo.alt_description,
 			title: photo.alt_description,
 		});
+
+		// Event listener check when each is finished loading
+
+		img.addEventListener('load', imageLoaded);
 
 		// put <img> inside <a> then put both inside imageContainer element
 
@@ -54,5 +76,16 @@ async function getPhotos() {
 		// Catch error here
 	}
 }
+
+// Check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+		ready = false;
+		getPhotos();
+		console.log(ready);
+		console.log('total images =', totalImages);
+		console.log('loaded images =', imagesLoaded);
+	}
+});
 
 getPhotos();
